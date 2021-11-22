2ef5223fa046c1b7b86c0724010a7a359c3591f9
Sergio <sergon149@alu.centroafuera.es>
2021-11-22 13:18:39 +0100

    casi terminado

--- app/src/main/java/com/example/ejerciciolisteners/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciolisteners/MainActivity.kt
@@ -2,19 +2,25 @@ package com.example.ejerciciolisteners
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import com.example.ejerciciolisteners.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         binding.b1.setOnClickListener {
-            var num = (1..100).random()
-            binding.tv1.text=num.toString()
+            var num = (Random.nextInt(99)+1)
+            binding.tv1.text= num.toString()
+            //
         }
 
         binding.b2.setOnClickListener {
@@ -23,6 +29,20 @@ class MainActivity : AppCompatActivity() {
 
         binding.b3.setOnClickListener {
             binding.tv1.text= palabraAleatoria()
+        }
+        binding.tv1.doOnTextChanged { text, start, before, count ->
+            if(count==5){
+                binding.b4.visibility = View.VISIBLE
+            }
+        }
+        binding.b4.setOnClickListener{
+            var letra = palabra.toCharArray()
+            var posicion = (Random.nextInt(4))
+            var aleatorio = Random.nextInt(97, 122).toChar()
+
+            Toast.makeText(this,"Cambiamos la letra ${letra[posicion]} por ${aleatorio}",Toast.LENGTH_LONG).show()
+            palabra=palabra.replaceRange(posicion,posicion+1, aleatorio.toString())
+            binding.tv1.text=palabra
         }
 
     }
@@ -36,14 +56,14 @@ class MainActivity : AppCompatActivity() {
         var letra = (97..122).random()
         return letra.toChar()
     }
-
+    var palabra = ""
     fun palabraAleatoria() : String{
-        var aux = ""
+
         for (i in 1..5){
-            var palabra = (97..122).random()
-            aux+=palabra
+            var letra = (97..122).random().toChar()
+            palabra+=letra
         }
-        return aux
+        return palabra
     }
 
 
